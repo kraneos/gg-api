7b9747195dea7ee2020d8a5b03ded03ce3fa6a3a
Agustin Polo <poloagustin@gmail.com>
2016-07-26 20:14:17 -0300

    agregado de form registro TODO: estilos

--- index.js
+++ index.js
@@ -10,44 +10,44 @@ var path = require('path');
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
+    console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+    databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: process.env.APP_ID || 'myAppId',
+    masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+    liveQuery: {
+        classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    }
 });
 var dashboard = new ParseDashboard({
-  "apps": [
-    {
-      "serverURL": process.env.SERVER_URL || "http://localhost:1337/parse",
-      "appId": process.env.APP_ID || "myAppId",
-      "masterKey": process.env.MASTER_KEY || "myMasterKey",
-      "appName": process.env.APP_NAME || "MyApp"
-    }
-  ],
-  "users": [
-    {
-      "user": "apolo",
-      "pass": "seggu2016"
-    },
-    {
-      "user": "egentile",
-      "pass": "seggu2016"
-    }
-  ]
+    "apps": [
+        {
+            "serverURL": process.env.SERVER_URL || "http://localhost:1337/parse",
+            "appId": process.env.APP_ID || "myAppId",
+            "masterKey": process.env.MASTER_KEY || "myMasterKey",
+            "appName": process.env.APP_NAME || "MyApp"
+        }
+    ],
+    "users": [
+        {
+            "user": "apolo",
+            "pass": "seggu2016"
+        },
+        {
+            "user": "egentile",
+            "pass": "seggu2016"
+        }
+    ]
 }, true);
 
 var register = require('./routes/register')(
-  process.env.SERVER_URL || "http://localhost:1337/parse",
-  process.env.APP_ID || "myAppId",
-  process.env.MASTER_KEY || "myMasterKey");
+    process.env.SERVER_URL || "http://localhost:1337/parse",
+    process.env.APP_ID || "myAppId",
+    process.env.MASTER_KEY || "myMasterKey");
 
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -57,6 +57,9 @@ var app = express();
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// Use CORS
+app.use(require('./cors'));
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
@@ -73,19 +76,19 @@ app.use(registerMountPath, register);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function (req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+    res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
 app.get('/test', function (req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+    res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function () {
-  console.log('parse-server-example running on port ' + port + '.');
+    console.log('parse-server-example running on port ' + port + '.');
 });
 
 // This will enable the Live Query real-time server
--- routes/register.js
+++ routes/register.js
@@ -1,70 +1,70 @@
 module.exports = function (serverUrl, appId, masterKey) {
-  var express = require('express');
+    var express = require('express');
 
-  var router = express.Router();
+    var router = express.Router();
 
-  router.post('/', function(req,res){
-    var Parse = require('parse/node');
-    Parse.initialize(appId, null, masterKey);
-    Parse.Cloud.useMasterKey();
-    Parse.serverURL = serverUrl;
-    var SegguClient = Parse.Object.extend('SegguClient');
+    router.post('/', function (req, res) {
+        var Parse = require('parse/node');
+        Parse.initialize(appId, null, masterKey);
+        Parse.Cloud.useMasterKey();
+        Parse.serverURL = serverUrl;
+        var SegguClient = Parse.Object.extend('SegguClient');
 
-    new Parse.Query(SegguClient)
-    .equalTo('name', req.body.company)
-    .find({
-      success: function (clients) {
-        if (clients.length > 0) {
-          throwNewError('Ya se hizo un usuario para esa empresa.');
-        } else {
-          createSegguClient(req, SegguClient);
-        }
-      },
-      error: throwError
-    });
+        new Parse.Query(SegguClient)
+            .equalTo('name', req.body.company)
+            .find({
+                success: function (clients) {
+                    if (clients.length > 0) {
+                        throwNewError('Ya se hizo un usuario para esa empresa.');
+                    } else {
+                        createSegguClient(req, SegguClient);
+                    }
+                },
+                error: throwError
+            });
 
-    function createSegguClient(req, SegguClient) {
-      var segguClient = new SegguClient();
-      segguClient.set('name', req.body.company);
+        function createSegguClient(req, SegguClient) {
+            var segguClient = new SegguClient();
+            segguClient.set('name', req.body.company);
 
-      segguClient.save().then(function (savedClient) {
-        var clientRole = getNewRole(req.body.company);
-        var clientClientsRole = getNewRole(req.body.company + 'Clients');
-        var roles = [clientRole, clientClientsRole];
-        Parse.Object.saveAll(roles).then(function (savedRoles) {
-          var parseUser = new Parse.User();
-          parseUser.set('username', req.body.username);
-          parseUser.set('password', req.body.password);
-          parseUser.set('email', req.body.email);
-          parseUser.set('phone', req.body.phone);
-          parseUser.set('segguClient', savedClient);
+            segguClient.save().then(function (savedClient) {
+                var clientRole = getNewRole(req.body.company);
+                var clientClientsRole = getNewRole(req.body.company + 'Clients');
+                var roles = [clientRole, clientClientsRole];
+                Parse.Object.saveAll(roles).then(function (savedRoles) {
+                    var parseUser = new Parse.User();
+                    parseUser.set('username', req.body.username);
+                    parseUser.set('password', req.body.password);
+                    parseUser.set('email', req.body.email);
+                    parseUser.set('phone', req.body.phone);
+                    parseUser.set('segguClient', savedClient);
 
-          parseUser.signUp().then(function (createdUser) {
-            var savedRole = savedRoles[0];
-            savedRole.getUsers().add(createdUser);
-            savedRole.save().then(function () {
-              console.log("Agregue los roles");
-              res.send("user created");
+                    parseUser.signUp().then(function (createdUser) {
+                        var savedRole = savedRoles[0];
+                        savedRole.getUsers().add(createdUser);
+                        savedRole.save().then(function () {
+                            console.log("Agregue los roles");
+                            res.send("user created");
+                        }, throwError);
+                    }, throwError);
+                });
+
+                function getNewRole(roleName, segguClient) {
+                    var roleACL = new Parse.ACL();
+                    roleACL.setPublicReadAccess(true);
+                    var role = new Parse.Role(roleName, roleACL);
+                    return role;
+                }
             }, throwError);
-          }, throwError);
-        });
-
-        function getNewRole(roleName, segguClient) {
-          var roleACL = new Parse.ACL();
-          roleACL.setPublicReadAccess(true);
-          var role = new Parse.Role(roleName, roleACL);
-          return role;
         }
-      }, throwError);
-    }
 
-    function throwError () {
-      next(new Error('El usuario no se pudo crear.'));
-    }
+        function throwError() {
+            res.status(500).send('El usuario no se pudo crear.');
+        }
 
-    function throwNewError (message) {
-      next(new Error(message));
-    }
-  });
-  return router;
+        function throwNewError(message) {
+            res.status(500).send(message);
+        }
+    });
+    return router;
 };
